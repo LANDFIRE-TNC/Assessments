b46366145d00953ff283c8ef357403c672a8a8b9
Randy Swaty <39503604+rswaty@users.noreply.github.com>
2025-02-13 15:53:01 -0500

    added map play

---
+++ map_play.R
@@ -0,0 +1,59 @@
+
+
+
+
+
+library(flexdashboard)
+library(leaflet)
+library(sf)
+library(leaflet.extras)
+library(htmltools)
+
+
+# Load the shapefile
+
+assessments_shp <- st_read("data/assessments.shp") %>%
+  st_transform(crs = 4326)
+
+
+
+
+tol_muted_palette <- colorFactor(c('#88CCEE', '#44AA99', '#117733', '#332288', '#DDCC77', '#999933','#CC6677', '#882255', '#AA4499', '#52504a'), assessments_shp$org_nam) # from https://zenodo.org/records/3381072
+
+# Create the Leaflet map
+
+
+# Create the Leaflet map
+# Define the color palette
+tol_muted_palette <- colorFactor(c('#88CCEE', '#44AA99', '#117733', '#332288', '#DDCC77', '#999933','#CC6677', '#882255', '#AA4499', '#52504a'), assessments_shp$org_nam) # from https://zenodo.org/records/3381072
+
+
+
+# Create the map
+map <- leaflet() %>%
+  addTiles() %>%
+  addPolygons(
+    data = assessments_shp, 
+    color = ~tol_muted_palette(org_nam), 
+    weight = 2, 
+    opacity = 1, 
+    fillOpacity = 0.3,
+    popup = ~paste(
+      "<b>Landscape:</b> ", dsply_n, "<br>",
+      "<b>Click for full report:</b> <a href='", link, "' target='_blank'>", link, "</a>"
+    ),
+    group = ~org_nam
+  ) %>%
+  addLayersControl(
+    overlayGroups = unique(assessments_shp$dsply_n),
+    options = layersControlOptions(collapsed = FALSE)
+  ) %>%
+  addLegend(
+    position = "bottomleft",  # Move legend to the left
+    pal = tol_muted_palette,
+    values = ~org_nam,
+    title = "Legend"
+  ) %>%
+  setView(lng = -96, lat = 37.8, zoom = 4)
+
+map
